290190b58c49d2e6943c346023f2f930633f603f
Ocap <160647584+OcapNese@users.noreply.github.com>
2024-02-26 20:38:51 -0600

    Add files via upload
    Diez treinta y nueve

--- Blue Moon State/data/scripts/BlueMoonPlugin.java
+++ Blue Moon State/data/scripts/BlueMoonPlugin.java
@@ -6,16 +6,25 @@ import com.fs.starfarer.api.campaign.*;
 import com.fs.starfarer.api.campaign.econ.EconomyAPI;
 import com.fs.starfarer.api.campaign.econ.MarketAPI;
 import com.fs.starfarer.api.campaign.econ.MarketAPI.SurveyLevel;
+import com.fs.starfarer.api.campaign.rules.MemoryAPI;
 import com.fs.starfarer.api.characters.PersonAPI;
-import com.fs.starfarer.api.characters.RelationshipAPI;
+import com.fs.starfarer.api.impl.campaign.DerelictShipEntityPlugin;
+import com.fs.starfarer.api.impl.campaign.fleets.FleetFactoryV3;
+import com.fs.starfarer.api.impl.campaign.fleets.FleetParamsV3;
 import com.fs.starfarer.api.impl.campaign.ids.*;
+import com.fs.starfarer.api.impl.campaign.missions.hub.BaseHubMission;
 import com.fs.starfarer.api.impl.campaign.procgen.StarAge;
 import com.fs.starfarer.api.impl.campaign.procgen.StarSystemGenerator;
-import com.fs.starfarer.api.impl.campaign.rulecmd.AddPotentialContact;
+import com.fs.starfarer.api.ui.SectorMapAPI;
+import com.fs.starfarer.api.ui.TooltipMakerAPI;
+import com.fs.starfarer.api.util.Misc;
+
+import java.awt.*;
 
 import static com.fs.starfarer.api.campaign.PersonImportance.*;
 import static com.fs.starfarer.api.characters.FullName.Gender.FEMALE;
 import static com.fs.starfarer.api.characters.FullName.Gender.MALE;
+import static com.fs.starfarer.api.impl.campaign.ids.FleetTypes.SCAVENGER_SMALL;
 import static com.fs.starfarer.api.impl.campaign.ids.Ranks.CITIZEN;
 import static com.fs.starfarer.api.impl.campaign.ids.Ranks.FACTION_LEADER;
 
@@ -23,10 +32,10 @@ public class BlueMoonPlugin extends BaseModPlugin {
 	@Override
 	public void onNewGame() {
 		SectorAPI sector = Global.getSector();
-		StarSystemAPI system = sector.createStarSystem("condilar");
+		StarSystemAPI system = sector.createStarSystem("Condilar");
 
-		PlanetAPI star = system.initStar(
-				"condilar",
+		PlanetAPI Sens = system.initStar(
+				"Condilar",
 				"star_yellow",
 				700,
 				25427,
@@ -37,30 +46,31 @@ public class BlueMoonPlugin extends BaseModPlugin {
 				"Condilar Buoy", // name - if null, defaultName from custom_entities.json will be used
 				"nav_buoy", // type of object, defined in custom_entities.json
 				"blue_moon"); // faction
-		buoy.setCircularOrbitPointingDown(star, 40, 2000, 70);
+		buoy.setCircularOrbitPointingDown(Sens, 40, 2000, 70);
 
 		SectorEntityToken relay = system.addCustomEntity("condilar_relay", // unique id
 				"Condilar Relay", // name - if null, defaultName from custom_entities.json will be used
 				"comm_relay", // type of object, defined in custom_entities.json
 				"blue_moon"); // faction
-		relay.setCircularOrbitPointingDown(star, 190, 3000, 70);
+		relay.setCircularOrbitPointingDown(Sens, 190, 3000, 70);
 
 		SectorEntityToken array = system.addCustomEntity("condilar_array", // unique id
 				"Condilar Array", // name - if null, defaultName from custom_entities.json will be used
 				"sensor_array", // type of object, defined in custom_entities.json
 				"blue_moon"); // faction
-		array.setCircularOrbitPointingDown(star, 220, 2000, 70);
+		array.setCircularOrbitPointingDown(Sens, 220, 2000, 70);
 
 		SectorEntityToken condilar_gate = system.addCustomEntity("condilar_gate", // unique id
 				"Condilar Gate", // name - if null, defaultName from custom_entities.json will be used
 				"inactive_gate", // type of object, defined in custom_entities.json
 				null); // faction
-		condilar_gate.setCircularOrbit(star, 120, 2500, 70);
+		condilar_gate.setCircularOrbit(Sens, 120, 2500, 70);
 
-		PlanetAPI Phantu = system.addPlanet("Phantu", star, "Phantu", "gas_giant", 230, 350, 5000, 150);
-		Phantu.setFaction("blue_moon");
-		MarketAPI phantu_market = Global.getFactory().createMarket("phantu_market", Phantu.getName(), 3);
+		PlanetAPI Bluemoon_Phantu_planet = system.addPlanet("Phantu", Sens, "Phantu", "gas_giant", 230, 350, 5000, 150);
+		Bluemoon_Phantu_planet.setFaction("blue_moon");
+		MarketAPI phantu_market = Global.getFactory().createMarket("phantu_market", Bluemoon_Phantu_planet.getName(), 3);
 
+		Bluemoon_Phantu_planet.setCustomDescriptionId("Bluemoon_Phantu_planet");
 		phantu_market.setPlanetConditionMarketOnly(false); //This "market" only represents planet conditions.
 		phantu_market.addCondition(Conditions.VERY_COLD); //It's a hot Jupiter so let's make it hot!
 		phantu_market.addCondition(Conditions.DENSE_ATMOSPHERE); //It's a gas giant, so let's make it gassy!
@@ -70,8 +80,8 @@ public class BlueMoonPlugin extends BaseModPlugin {
 		phantu_market.addCondition(Conditions.ORE_RICH);
 		phantu_market.addCondition(Conditions.RARE_ORE_RICH);
 		phantu_market.addCondition(Conditions.ORGANICS_ABUNDANT);
-		phantu_market.setPrimaryEntity(Phantu); //Tell the "market" that it's on our planet.
-		Phantu.setMarket(phantu_market); //Likewise, tell our planet that it has a "market".
+		phantu_market.setPrimaryEntity(Bluemoon_Phantu_planet); //Tell the "market" that it's on our planet.
+		Bluemoon_Phantu_planet.setMarket(phantu_market); //Likewise, tell our planet that it has a "market".
 		phantu_market.setSurveyLevel(SurveyLevel.FULL);
 		phantu_market.getTariff().modifyFlat("generator", 0.3f);
 		phantu_market.setPlanetConditionMarketOnly(false); //This "market" only represents planet conditions.
@@ -87,10 +97,11 @@ public class BlueMoonPlugin extends BaseModPlugin {
 		phantu_market.addSubmarket(Submarkets.SUBMARKET_OPEN);
 
 
-		PlanetAPI estoul = system.addPlanet("estoul", Phantu, "Estoul", "jungle", 10, 80, 700, 250);
-		estoul.setFaction("blue_moon");
-		MarketAPI Estoul_market = Global.getFactory().createMarket("Estoul_market", estoul.getName(), 5);
+		PlanetAPI Bluemoon_Estoul_planet = system.addPlanet("Estoul", Bluemoon_Phantu_planet, "Estoul", "jungle", 10, 80, 700, 250);
+		Bluemoon_Estoul_planet.setFaction("blue_moon");
+		MarketAPI Estoul_market = Global.getFactory().createMarket("Estoul_market", Bluemoon_Estoul_planet.getName(), 5);
 
+		Bluemoon_Estoul_planet.setCustomDescriptionId("Bluemoon_Estoul_planet");
 		Estoul_market.addCondition(Conditions.HABITABLE);
 		Estoul_market.addCondition(Conditions.COLD);
 		Estoul_market.addCondition(Conditions.ORE_RICH);
@@ -98,8 +109,8 @@ public class BlueMoonPlugin extends BaseModPlugin {
 		Estoul_market.addCondition(Conditions.ORGANICS_TRACE);
 		Estoul_market.addCondition(Conditions.VOLATILES_DIFFUSE);
 		Estoul_market.addCondition(Conditions.FARMLAND_BOUNTIFUL);
-		Estoul_market.setPrimaryEntity(estoul); //Tell the "market" that it's on our planet.
-		estoul.setMarket(Estoul_market); //Likewise, tell our planet that it has a "market".
+		Estoul_market.setPrimaryEntity(Bluemoon_Estoul_planet); //Tell the "market" that it's on our planet.
+		Bluemoon_Estoul_planet.setMarket(Estoul_market); //Likewise, tell our planet that it has a "market".
 		Estoul_market.setSurveyLevel(SurveyLevel.FULL);
 		Estoul_market.getTariff().modifyFlat("generator", 0.4f);
 		Estoul_market.setPlanetConditionMarketOnly(false); //This "market" only represents planet conditions.
@@ -118,18 +129,19 @@ public class BlueMoonPlugin extends BaseModPlugin {
 		Estoul_market.addSubmarket(Submarkets.SUBMARKET_OPEN);
 		Estoul_market.addSubmarket(Submarkets.GENERIC_MILITARY);
 
-		PlanetAPI Moonial = system.addPlanet("moonial", star, "Moonial", "tundra", 570, 175, 7000, 9000);
-		Moonial.setFaction("blue_moon");
-		MarketAPI Moonial_market = Global.getFactory().createMarket("Moonial_market", Moonial.getName(), 7);
+		PlanetAPI Bluemoon_Moonial_planet = system.addPlanet("Moonial", Sens, "Moonial", "tundra", 570, 175, 7000, 9000);
+		Bluemoon_Moonial_planet.setFaction("blue_moon");
+		MarketAPI Moonial_market = Global.getFactory().createMarket("Moonial_market", Bluemoon_Moonial_planet.getName(), 7);
 
+		Bluemoon_Moonial_planet.setCustomDescriptionId("Bluemoon_Moonial_planet");
 		Moonial_market.setPlanetConditionMarketOnly(false); //This "market" only represents planet conditions.
 		Moonial_market.addCondition(Conditions.COLD);
 		Moonial_market.addCondition(Conditions.THIN_ATMOSPHERE); //It's a gas giant, so let's make it gassy!
 		Moonial_market.addCondition(Conditions.MILD_CLIMATE);
 		Moonial_market.addCondition(Conditions.LOW_GRAVITY);
 		Moonial_market.addCondition(Conditions.RUINS_EXTENSIVE);
-		Moonial_market.setPrimaryEntity(Moonial); //Tell the "market" that it's on our planet.
-		Moonial.setMarket(Moonial_market); //Likewise, tell our planet that it has a "market".
+		Moonial_market.setPrimaryEntity(Bluemoon_Moonial_planet); //Tell the "market" that it's on our planet.
+		Bluemoon_Moonial_planet.setMarket(Moonial_market); //Likewise, tell our planet that it has a "market".
 		Moonial_market.setSurveyLevel(SurveyLevel.FULL);
 		Moonial_market.getTariff().modifyFlat("generator", 0.3f);
 		Moonial_market.setPlanetConditionMarketOnly(false); //This "market" only represents planet conditions.
@@ -165,8 +177,7 @@ public class BlueMoonPlugin extends BaseModPlugin {
 			Ast.setPostId(FACTION_LEADER);
 
 			Ast.getStats().setSkillLevel(Skills.INDUSTRIAL_PLANNING, 1);
-			Ast.getStats().setSkillLevel(Skills.SPACE_OPERATIONS, 2);
-			Ast.getStats().setSkillLevel(Skills.PLANETARY_OPERATIONS, 2);
+
 
 
 			Moonial_market.getCommDirectory().addPerson(Ast, 1);
@@ -178,18 +189,19 @@ public class BlueMoonPlugin extends BaseModPlugin {
 
 
 
-		PlanetAPI ethoi = system.addPlanet("Ethoi", star, "Ethoi", "arid", 570, 175, 7500, 9700);
-		ethoi.setFaction("blue_moon");
-		MarketAPI Ethoi_market = Global.getFactory().createMarket("Ethoi_market", ethoi.getName(), 4);
+		PlanetAPI Bluemoon_Ethoi_planet = system.addPlanet("Ethoi", Sens, "Ethoi", "arid", 570, 175, 7500, 9700);
+		Bluemoon_Ethoi_planet.setFaction("blue_moon");
+		MarketAPI Ethoi_market = Global.getFactory().createMarket("Ethoi_market", Bluemoon_Ethoi_planet.getName(), 4);
 
+		Bluemoon_Ethoi_planet.setCustomDescriptionId("Bluemoon_Ethoi_planet");
 		Ethoi_market.setPlanetConditionMarketOnly(false); //This "market" only represents planet conditions.
 		Ethoi_market.addCondition(Conditions.HOT);
 		Ethoi_market.addCondition(Conditions.TOXIC_ATMOSPHERE); //It's a gas giant, so let's make it gassy!
 		Ethoi_market.addCondition(Conditions.MILD_CLIMATE);
 		Ethoi_market.addCondition(Conditions.LOW_GRAVITY);
 		Ethoi_market.addCondition(Conditions.HABITABLE);
-		Ethoi_market.setPrimaryEntity(ethoi); //Tell the "market" that it's on our planet.
-		ethoi.setMarket(Ethoi_market); //Likewise, tell our planet that it has a "market".
+		Ethoi_market.setPrimaryEntity(Bluemoon_Ethoi_planet); //Tell the "market" that it's on our planet.
+		Bluemoon_Ethoi_planet.setMarket(Ethoi_market); //Likewise, tell our planet that it has a "market".
 		Ethoi_market.setSurveyLevel(SurveyLevel.FULL);
 		Ethoi_market.getTariff().modifyFlat("generator", 0.3f);
 		Ethoi_market.setPlanetConditionMarketOnly(false); //This "market" only represents planet conditions.
@@ -205,17 +217,18 @@ public class BlueMoonPlugin extends BaseModPlugin {
 		Ethoi_market.addSubmarket(Submarkets.SUBMARKET_BLACK);
 		Ethoi_market.addSubmarket(Submarkets.SUBMARKET_OPEN);
 
-		PlanetAPI Seduveren = system.addPlanet("Seduveren", star, "Seduveren", "jungle", 30, 50, 6300, 1200);
-		Seduveren.setFaction("oda_clan");
-		MarketAPI seduveren_market = Global.getFactory().createMarket("seduveren_market", Seduveren.getName(), 4);
+		PlanetAPI Bluemoon_Seduveren_planet = system.addPlanet("Seduveren", Sens, "Seduveren", "jungle", 30, 50, 6300, 1200);
+		Bluemoon_Seduveren_planet.setFaction("oda_clan");
+		MarketAPI seduveren_market = Global.getFactory().createMarket("seduveren_market", Bluemoon_Seduveren_planet.getName(), 4);
 
+		Bluemoon_Seduveren_planet.setCustomDescriptionId("Bluemoon_Seduveren_planet");
 		seduveren_market.setPlanetConditionMarketOnly(false); //This "market" only represents planet conditions.
 		seduveren_market.addCondition(Conditions.MILD_CLIMATE); //It's a hot Jupiter so let's make it hot!
 		seduveren_market.addCondition(Conditions.THIN_ATMOSPHERE); //It's a gas giant, so let's make it gassy!
 		seduveren_market.addCondition(Conditions.WATER);
 		seduveren_market.addCondition(Conditions.FARMLAND_BOUNTIFUL);
-		seduveren_market.setPrimaryEntity(Seduveren); //Tell the "market" that it's on our planet.
-		Seduveren.setMarket(seduveren_market); //Likewise, tell our planet that it has a "market".
+		seduveren_market.setPrimaryEntity(Bluemoon_Seduveren_planet); //Tell the "market" that it's on our planet.
+		Bluemoon_Seduveren_planet.setMarket(seduveren_market); //Likewise, tell our planet that it has a "market".
 		seduveren_market.setSurveyLevel(SurveyLevel.FULL);
 		seduveren_market.getTariff().modifyFlat("generator", 0.0f);
 		seduveren_market.addCondition(Conditions.POPULATION_4);
@@ -244,8 +257,6 @@ public class BlueMoonPlugin extends BaseModPlugin {
 			Ocp.setPostId(CITIZEN);
 
 			Ocp.getStats().setSkillLevel(Skills.INDUSTRIAL_PLANNING, 1);
-			Ocp.getStats().setSkillLevel(Skills.SPACE_OPERATIONS, 2);
-			Ocp.getStats().setSkillLevel(Skills.PLANETARY_OPERATIONS, 2);
 
 
 			seduveren_market.getCommDirectory().addPerson(Ocp, 9);
@@ -265,8 +276,6 @@ public class BlueMoonPlugin extends BaseModPlugin {
 			Oda.setPostId(FACTION_LEADER);
 
 			Oda.getStats().setSkillLevel(Skills.INDUSTRIAL_PLANNING, 1);
-			Oda.getStats().setSkillLevel(Skills.SPACE_OPERATIONS, 2);
-			Oda.getStats().setSkillLevel(Skills.PLANETARY_OPERATIONS, 2);
 
 
 			seduveren_market.getCommDirectory().addPerson(Oda, 1);
@@ -283,10 +292,10 @@ public class BlueMoonPlugin extends BaseModPlugin {
 
 		StarSystemGenerator.addOrbitingEntities(
 				system,
-				star,
+				Sens,
 				StarAge.AVERAGE, //Let's try old system entities this time.
 				6, 9, //Let's allow for the possibility of no new entities after Aaarg.
-				ethoi.getCircularOrbitRadius() + 600, //Here we grab Steam's orbit radius to figure out where to start adding from.
+				Bluemoon_Ethoi_planet.getCircularOrbitRadius() + 600, //Here we grab Steam's orbit radius to figure out where to start adding from.
 				system.getPlanets().size(), //Let's start naming planets based off the number of planets already in this location.
 				false); // Again, let's use generic planet names.
 
@@ -309,5 +318,6 @@ public class BlueMoonPlugin extends BaseModPlugin {
 		blue_moon.setRelationship("oda_clan", 0.8f);
 		blue_moon.setRelationship("player", 0.1f);
 		}
-	}
+
+}
 
